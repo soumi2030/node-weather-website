57e2cb9d22c15829a041c240b786afcc78b85179
Soumi Lahiri <soumi2030@gmail.com>
2020-05-01 09:23:51 +0530

    modification for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit',(e)=>
     e.preventDefault();
     const location = search.value;
     console.log(location);
-    fetch('http://localhost:3000/weather?address=' +location).then((response)=> {     
+    fetch('/weather?address=' +location).then((response)=> {     
     response.json().then((data) => {
         console.log(data)
         message1.textContent ='loading'
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path =require('path');
 const app= express();
+const port =process.env.PORT || 3000;
 const geocode = require('../util/geocode');
 const forecast = require('../util/forecast');
 
@@ -66,7 +67,7 @@ app.get('', (req, res)=>
 
 
 
-    app.listen(3000, ()=>
+    app.listen(port, ()=>
     {
-        console.log('app listening on port 3000')
+        console.log('app listening on port '+port)
     })
